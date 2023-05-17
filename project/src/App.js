34ca8012858aff831217716a9c4f27bbd01bca0e
yooni <jiyoon0616@naver.com>
2023-05-18 00:28:44 +0900

    feat (App) : Footer 컴포넌트 추가

--- project/src/App.js
+++ project/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Component/Header';
 import MainPage from './Page/MainPage';
+import Footer from './Component/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
